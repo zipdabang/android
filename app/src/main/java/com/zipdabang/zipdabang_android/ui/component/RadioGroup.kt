ad9f477f6b031f2bbc5e646cd936f0e7edd74f7a
Kim Gimun <kmkim7575@gmail.com>
2023-07-24 16:47:33 +0900

    [FEAT] radio group implementation

---
+++ app/src/main/java/com/zipdabang/zipdabang_android/ui/component/RadioGroup.kt
@@ -0,0 +1,145 @@
+package com.zipdabang.zipdabang_android.ui.component
+
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.zipdabang.zipdabang_android.R
+import com.zipdabang.zipdabang_android.ui.theme.ZipdabangandroidTheme
+
+@Composable
+fun RadioGroupHorizontal(
+    optionList: List<String>,
+    onOptionChange: (String) -> Unit
+) {
+    var selectedOption by remember {
+        mutableStateOf(optionList[0])
+    }
+
+    Row(
+        modifier = Modifier
+            .selectableGroup()
+            .fillMaxWidth()
+    ) {
+        optionList.forEach { option ->
+            Row(
+                modifier = Modifier
+                    .selectable(
+                        selected = (selectedOption == option),
+                        onClick = {
+                            selectedOption = option
+                            onOptionChange(selectedOption)
+                        },
+                        role = Role.RadioButton
+                    )
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(
+                        id = if (selectedOption == option) R.drawable.ic_radio_checked else R.drawable.ic_radio_unchecked
+                    ),
+                    contentDescription = "radio",
+                    tint = ZipdabangandroidTheme.Colors.Strawberry
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Text(text = option, textAlign = TextAlign.Center)
+                
+            }
+        }
+        
+    }
+}
+
+@Composable
+fun RadioGroupVertical(
+    optionList: List<String>,
+    onOptionChange: (String) -> Unit
+) {
+    var selectedOption by remember {
+        mutableStateOf(optionList[0])
+    }
+
+    Column(
+        modifier = Modifier
+            .selectableGroup()
+            .fillMaxWidth()
+    ) {
+        optionList.forEach { option ->
+            Row(
+                modifier = Modifier
+                    .selectable(
+                        selected = (selectedOption == option),
+                        onClick = {
+                            selectedOption = option
+                            onOptionChange(selectedOption)
+                        },
+                        role = Role.RadioButton
+                    )
+                    .padding(20.dp)
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    painter = painterResource(
+                        id = if (selectedOption == option) R.drawable.ic_radio_checked else R.drawable.ic_radio_unchecked
+                    ),
+                    contentDescription = "radio",
+                    tint = ZipdabangandroidTheme.Colors.Strawberry
+                )
+
+                Spacer(modifier = Modifier.width(8.dp))
+
+                Text(text = option, textAlign = TextAlign.Center)
+
+            }
+
+            Spacer(modifier = Modifier.width(12.dp))
+            Divider(
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .align(Alignment.CenterHorizontally),
+            )
+        }
+
+    }
+}
+
+
+@Preview
+@Composable
+fun RadioGroupHorizontalPreview() {
+    RadioGroupHorizontal(optionList = listOf("카드", "현금", "수표"), onOptionChange = {})
+}
+
+@Preview
+@Composable
+fun RadioGroupVerticalPreview() {
+    RadioGroupVertical(optionList = listOf("카드", "현금", "수표"), onOptionChange = {})
+}
