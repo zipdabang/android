724e6f9502caf082f988b70391bb1f471ec55de3
Kim Gimun <kmkim7575@gmail.com>
2023-08-10 22:15:10 +0900

    :art: [FIX] use case unification & google authentication error fix
    the same authentication function in viewmodel for getting the token of zipdabang

--- app/src/main/java/com/zipdabang/zipdabang_android/module/login/ui/LoginViewModel.kt
+++ app/src/main/java/com/zipdabang/zipdabang_android/module/login/ui/LoginViewModel.kt
@@ -7,8 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zipdabang.zipdabang_android.common.Resource
 import com.zipdabang.zipdabang_android.module.login.data.AuthBody
-import com.zipdabang.zipdabang_android.module.login.use_case.GetGoogleAuthResultUseCase
-import com.zipdabang.zipdabang_android.module.login.use_case.GetKakaoAuthResultUseCase
+import com.zipdabang.zipdabang_android.module.login.use_case.GetAuthResultUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -16,8 +15,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor (
-    private val getKakaoAuthResultUseCase: GetKakaoAuthResultUseCase,
-    private val getGoogleAuthResultUseCase: GetGoogleAuthResultUseCase
+    private val getAuthResultUseCase: GetAuthResultUseCase
 ) : ViewModel() {
     companion object {
         const val TAG = "LoginViewModel"
@@ -37,8 +35,8 @@ class LoginViewModel @Inject constructor (
     private val _state = mutableStateOf(AuthState())
     val state: State<AuthState> = _state
 
-    fun getKakaoAuthResult(body: AuthBody) {
-        getKakaoAuthResultUseCase(body).onEach { result ->
+    fun getAuthResult(body: AuthBody, platform: String) {
+        getAuthResultUseCase(body, platform).onEach { result ->
             // 별 일이 없으면, 첫 번째로는 Resource.Loading이 발행되고, 두 번째로는 Resource.success가 발행.
             // 발행 중 문제가 생기면 Resource.Error 발행
             when (result) {
@@ -64,7 +62,7 @@ class LoginViewModel @Inject constructor (
             }
         }.launchIn(viewModelScope)
     }
-
+/*
     fun getGoogleAuthResult(body: AuthBody) {
         getGoogleAuthResultUseCase(body).onEach { result ->
             // 별 일이 없으면, 첫 번째로는 Resource.Loading이 발행되고, 두 번째로는 Resource.success가 발행.
@@ -88,5 +86,5 @@ class LoginViewModel @Inject constructor (
                 }
             }
         }.launchIn(viewModelScope)
-    }
+    }*/
 }
