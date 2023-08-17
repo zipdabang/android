dd9446eafcb2e775c83b1aeb88d383da0da8be31
Kim Gimun <kmkim7575@gmail.com>
2023-08-17 23:12:32 +0900

    :sparkles: [FEAT] Deriving FCM token from Firebase + storing token into datastore
    fcm 기능에 필요한 토큰을 파이어베이스 서버로부터 가져오고, 이를 로컬 저장소에 저장하는 기능을 구현

--- app/src/main/java/com/zipdabang/zipdabang_android/module/main/MainActivity.kt
+++ app/src/main/java/com/zipdabang/zipdabang_android/module/main/MainActivity.kt
@@ -1,9 +1,21 @@
 package com.zipdabang.zipdabang_android.module.main
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import android.Manifest
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.LaunchedEffect
+import androidx.core.content.ContextCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.messaging.ktx.messaging
+import com.zipdabang.zipdabang_android.core.data_store.ProtoDataViewModel
 import com.zipdabang.zipdabang_android.core.data_store.test.DataStoreTestScreen
 import com.zipdabang.zipdabang_android.core.navigation.RootNavGraph
 import com.zipdabang.zipdabang_android.module.login.ui.LoginScreen
@@ -12,6 +24,12 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        private const val TAG = "MainActivity"
+        private const val NOTIFICATION_REQUEST_CODE = 1234
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,6 +39,66 @@ class MainActivity : ComponentActivity() {
                 RootNavGraph(navController = navController)
 
             }
+
+            val tokenDataViewModel = hiltViewModel<ProtoDataViewModel>()
+            registerDerivedToken(tokenDataViewModel)
+
         }
+    }
+
+    private fun askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted: Boolean ->
+        if (isGranted) {
+            // FCM SDK (and your app) can post notifications.
+        } else {
+            // TODO: Inform user that that your app will not show notifications.
+        }
+    }
+
+    private fun registerDerivedToken(viewModel: ProtoDataViewModel) {
+        // [START log_reg_token]
+        Firebase.messaging.token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w(TAG, "Fetching FCM registration token failed", task.exception)
+                return@addOnCompleteListener
+            }
+
+            // Get new FCM registration token
+            val token = task.result
+
+            // 저장소에 업데이트
+            viewModel.updateFcmToken(token)
+
+            // 서버로 전송
+
+            // Log and toast
+            val msg = "FCM Registration token: $token"
+            Log.d(TAG, msg)
+
+
+
+            // TODO 토큰 서버에 전송 api
+        }
+        // [END log_reg_token]
     }
 }
