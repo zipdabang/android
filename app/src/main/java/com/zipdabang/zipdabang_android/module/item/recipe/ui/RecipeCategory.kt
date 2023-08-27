8a1e4093cbdfa41d7a442222534d495ede54e8d7
Kim Gimun <kmkim7575@gmail.com>
2023-08-27 23:39:20 +0900

    :lipstick: [FIX] Category Items Ripple Effect Implementation
    ripple effect when clicked

--- app/src/main/java/com/zipdabang/zipdabang_android/module/item/recipe/ui/RecipeCategory.kt
+++ app/src/main/java/com/zipdabang/zipdabang_android/module/item/recipe/ui/RecipeCategory.kt
@@ -8,6 +8,7 @@ import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,8 +19,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -44,7 +47,10 @@ fun RecipeCategory(
 ) {
     Column(
         modifier = Modifier
-            .clickable {
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple()
+            ) {
                 onClick(categoryId)
             }
             .padding(4.dp),
