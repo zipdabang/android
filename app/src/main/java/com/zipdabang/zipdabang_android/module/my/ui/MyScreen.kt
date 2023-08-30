1a7cd2dfe1fdfa253593059d1b445a379b6f8c27
jessy <yaeunjess@gmail.com>
2023-08-30 11:39:55 +0900

    commit for pull main

--- app/src/main/java/com/zipdabang/zipdabang_android/module/my/ui/MyScreen.kt
+++ app/src/main/java/com/zipdabang/zipdabang_android/module/my/ui/MyScreen.kt
@@ -1,6 +1,9 @@
 package com.zipdabang.zipdabang_android.module.my.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
@@ -14,9 +17,13 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import com.zipdabang.zipdabang_android.R
+import com.zipdabang.zipdabang_android.core.data_store.proto.ProtoDataViewModel
 import com.zipdabang.zipdabang_android.module.bottom.BottomMenuContent
 import com.zipdabang.zipdabang_android.module.bottom.ui.BottomNavigationBar
 import com.zipdabang.zipdabang_android.ui.component.AppBarDefault
@@ -31,6 +38,7 @@ fun MyScreen(){
     //drawer에 필요한 drawerState랑 scope
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val scope = rememberCoroutineScope()
+    //val tokenStoreViewModel = hiltViewModel<ProtoDataViewModel>()
 
     ModalDrawer(
         scaffold = {
@@ -46,11 +54,38 @@ fun MyScreen(){
                 containerColor = Color.White,
                 contentColor = Color.Black,
                 content = {
-                    Text(text="my", modifier = Modifier.padding(it))
+                    Surface(
+                        modifier = Modifier
+                            .padding(it)
+                            .fillMaxSize(),
+                        color = Color.White
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .background(Color.White)
+                                .fillMaxSize(),
+                        ) {
+                            Text(
+                                text="로그아웃",
+                                modifier = Modifier.clickable(
+                                    onClick = {
+                                        //tokenStoreViewModel.updateAccessToken("")
+                                        //tokenStoreViewModel.updateRefreshToken("")
+                                    }
+                                )
+                            )
+                        }
+                    }
                 }
             )
         },
         drawerState = drawerState
     )
 
+}
+
+@Preview
+@Composable
+fun PreviewMyScreen() {
+    MyScreen()
 }
