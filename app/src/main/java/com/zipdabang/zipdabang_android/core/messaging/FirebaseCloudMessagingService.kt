4aa3f91ae8f1802a9f64dc98036458c240c2ca15
Kim Gimun <kmkim7575@gmail.com>
2023-09-30 23:58:21 +0900

    :sparkles: [FEAT] FCM send notification with data payload + navigating to the specific screen when clicked
    fcm

--- app/src/main/java/com/zipdabang/zipdabang_android/core/messaging/FirebaseCloudMessagingService.kt
+++ app/src/main/java/com/zipdabang/zipdabang_android/core/messaging/FirebaseCloudMessagingService.kt
@@ -5,63 +5,73 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.datastore.core.DataStore
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil.request.SuccessResult
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.remoteMessage
 import com.zipdabang.zipdabang_android.R
+import com.zipdabang.zipdabang_android.common.Constants
+import com.zipdabang.zipdabang_android.core.data_store.proto.Token
 import com.zipdabang.zipdabang_android.module.main.MainActivity
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class FirebaseCloudMessagingService: FirebaseMessagingService() {
     companion object {
         private const val TAG = "MyFirebaseMsgService"
     }
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
-        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
-        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
-        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
-        // When the user taps on the notification they are returned to the app. Messages containing both notification
-        // and data payloads are treated as notification messages. The Firebase console always sends notification
-        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
-        // [END_EXCLUDE]
+    @Inject
+    lateinit var tokenDataStore: DataStore<Token>
 
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         Log.d(TAG, "From: ${remoteMessage.from}")
 
         // Check if message contains a data payload.
         if (remoteMessage.data.isNotEmpty()) {
             Log.d(TAG, "Message data payload: ${remoteMessage.data}")
+            Log.d(TAG, "Message notification payload: ${remoteMessage.notification}")
+
+            sendNotification(remoteMessage)
 
             // Check if data needs to be processed by long running job
-            if (isLongRunningJob()) {
+/*            if (isLongRunningJob()) {
                 // For long-running tasks (10 seconds or more) use WorkManager.
                 // scheduleJob()
             } else {
                 // Handle message within 10 seconds
                 handleNow()
-            }
+            }*/
         }
 
         // Check if message contains a notification payload.
-        remoteMessage.notification?.let {
+/*        remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
             it.body?.let { body -> sendNotification(body) }
-        }
+        }*/
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
     }
-    // [END receive_message]
 
     private fun isLongRunningJob() = true
 
-    // [START on_new_token]
     /**
      * Called if the FCM registration token is updated. This may occur if the security of
      * the previous token had been compromised. Note that this is called when the
@@ -70,9 +80,19 @@ class FirebaseCloudMessagingService: FirebaseMessagingService() {
     override fun onNewToken(token: String) {
         Log.d(TAG, "Refreshed token: $token")
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
+        CoroutineScope(Dispatchers.IO).launch {
+            Log.d(TAG, "update started")
+            if (tokenDataStore.data.first().fcmToken != null) {
+                tokenDataStore.updateData { tokens ->
+                    Log.d(TAG, "update in progress")
+                    tokens.copy(
+                        fcmToken = token
+                    )
+                }.fcmToken ?: Constants.TOKEN_NULL
+            }
+        }
+
+        // FCM registration token to app server.
         sendRegistrationToServer(token)
     }
     // [END on_new_token]
@@ -96,27 +116,20 @@ class FirebaseCloudMessagingService: FirebaseMessagingService() {
         Log.d(TAG, "Short lived task is done.")
     }
 
-    /**
-     * Persist token to third-party servers.
-     *
-     * Modify this method to associate the user's FCM registration token with any server-side account
-     * maintained by your application.
-     *
-     * @param token The new token.
-     */
     private fun sendRegistrationToServer(token: String?) {
         // TODO: Implement this method to send token to your app server.
         Log.d(TAG, "sendRegistrationTokenToServer($token)")
     }
 
-    /**
-     * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
-     */
-    private fun sendNotification(messageBody: String) {
+    private fun sendNotification(remoteMessage: RemoteMessage) {
         val requestCode = 0
+        val notificationId: Int = (System.currentTimeMillis() / 7).toInt()
         val intent = Intent(this, MainActivity::class.java)
+
+        for (key in remoteMessage.data.keys) {
+            intent.putExtra(key, remoteMessage.data.getValue(key))
+        }
+
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         val pendingIntent = PendingIntent.getActivity(
             this,
@@ -128,15 +141,6 @@ class FirebaseCloudMessagingService: FirebaseMessagingService() {
         val channelId = getString(R.string.default_notification_channel_id)
         val channelName = getString(R.string.default_notification_channel_name)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.drawable.zipdabanglogo_brown)
-            .setContentTitle(getString(R.string.fcm_message))
-            .setContentText(messageBody)
-            .setAutoCancel(true)
-            .setSound(defaultSoundUri)
-            // 메시지 클릭 시 이동할 화면 정의한 intent
-            .setContentIntent(pendingIntent)
-
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         // Since android Oreo notification channel is needed.
@@ -144,12 +148,35 @@ class FirebaseCloudMessagingService: FirebaseMessagingService() {
             val channel = NotificationChannel(
                 channelId,
                 channelName,
-                NotificationManager.IMPORTANCE_DEFAULT,
+                NotificationManager.IMPORTANCE_HIGH,
             )
             notificationManager.createNotificationChannel(channel)
         }
 
-        val notificationId = 0
+/*        CoroutineScope(Dispatchers.Main).launch {
+            val iconBitmap = getBitmap(remoteMessage.data[""].toString())
+        }*/
+
+        val notificationBuilder =
+            NotificationCompat.Builder(this@FirebaseCloudMessagingService, channelId)
+                .setSmallIcon(R.drawable.zipdabanglogo_brown)
+                .setContentTitle(remoteMessage.data["title"].toString())
+                .setContentText(remoteMessage.data["body"].toString())
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                // 메시지 클릭 시 이동할 화면 정의한 intent
+                .setContentIntent(pendingIntent)
+
         notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+
+    private suspend fun getBitmap(imageUrl: String): Bitmap = withContext(Dispatchers.IO) {
+        val loading = ImageLoader(this@FirebaseCloudMessagingService)
+        val request = ImageRequest.Builder(this@FirebaseCloudMessagingService)
+            .data(imageUrl)
+            .build()
+
+        val result = (loading.execute(request) as SuccessResult).drawable
+        (result as BitmapDrawable).bitmap
     }
 }
--- app/src/main/java/com/zipdabang/zipdabang_android/core/navigation/RootNavGraph.kt
+++ app/src/main/java/com/zipdabang/zipdabang_android/core/navigation/RootNavGraph.kt
@@ -1,29 +1,41 @@
 package com.zipdabang.zipdabang_android.core.navigation
 
 import android.os.Build
+import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.zipdabang.zipdabang_android.module.comment.ui.RecipeCommentViewModel
+import com.zipdabang.zipdabang_android.module.detail.recipe.ui.RecipeDetailScreen
+import com.zipdabang.zipdabang_android.module.main.FCMData
 import com.zipdabang.zipdabang_android.module.main.MainScreen
 import com.zipdabang.zipdabang_android.module.splash.ui.SplashScreen
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun RootNavGraph(
-    navController: NavHostController
+    outerNavController: NavHostController,
+    fcmData: FCMData? = null
 ) {
+
+    val innerNavController = rememberNavController()
+    Log.d("RootNavGraph", fcmData.toString())
+
     NavHost(
-        navController = navController,
+        navController = outerNavController,
         startDestination = SPLASH_ROUTE,
         route = ROOT_ROUTE
     ) {
         composable(SPLASH_ROUTE) {
             SplashScreen(
-                navController = navController,
                 onTokenValid = {
-                    navController.navigate(MAIN_ROUTE) {
+                    outerNavController.navigate(MAIN_ROUTE) {
                         popUpTo(SPLASH_ROUTE) {
                             inclusive = true
                         }
@@ -31,27 +43,73 @@ fun RootNavGraph(
                     }
                 },
                 onTokenInvalid = {
-                    navController.navigate(AuthScreen.SignIn.route) {
+                    outerNavController.navigate(AuthScreen.SignIn.route) {
                         popUpTo(SPLASH_ROUTE) {
                             inclusive = true
                         }
                         launchSingleTop = true
                     }
-                }
+                },
+                onNotificationClick = {
+                    fcmData?.let {
+                        when (it.targetCategory) {
+                            "레시피" -> {
+                                outerNavController.navigate(
+                                    route = SharedScreen.DetailRecipe.passRecipeId(it.targetId)
+                                ) {
+                                    popUpTo(SPLASH_ROUTE) {
+                                        inclusive = true
+                                    }
+                                    launchSingleTop = true
+                                }
+                            }
+                            else -> {
 
+                            }
+                        }
+                    }
+                },
+                fcmData = fcmData
             )
         }
 
-        authNavGraph(navController = navController)
+        authNavGraph(navController = outerNavController)
 
         composable(route = MAIN_ROUTE) {
            MainScreen(
-               outerNavController = navController
+               outerNavController = outerNavController,
+               innerNavController = innerNavController
            )
         }
 
+        composable(
+            route = SharedScreen.DetailRecipe.route,
+            arguments = listOf(
+                navArgument(name = "recipeId") { type = NavType.IntType }
+            )
+        ) { backStackEntry ->
 
+            val recipeCommentViewModel = hiltViewModel<RecipeCommentViewModel>()
 
-
+            RecipeDetailScreen(
+                navController = outerNavController,
+                recipeId = backStackEntry.arguments?.getInt("recipeId"),
+                onClickBackIcon = {},
+                onClickProfile = { ownerId -> },
+                onClickCart = { keyword -> },
+                onClickDelete = { recipeId -> },
+                onClickEdit = { recipeId -> },
+                onClickBlock = { userId -> },
+                onClickReport = { recipeId -> },
+                onClickCommentBlock = { recipeId -> },
+                onClickCommentReport = { recipeId -> },
+                onClickCommentDelete = { recipeId, commentId ->
+                    recipeCommentViewModel.deleteComment(recipeId, commentId)
+                },
+                onClickCommentEdit = { recipeId, commentId, newContent ->
+                    recipeCommentViewModel.editComment(recipeId, commentId, newContent)
+                },
+            )
+        }
     }
 }
