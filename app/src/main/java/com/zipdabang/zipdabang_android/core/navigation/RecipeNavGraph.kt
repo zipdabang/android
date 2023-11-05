6dcd8683bc89002a0b6122fd5ba9b3f690d95e7c
Gimun Kim <101035437+kmkim2689@users.noreply.github.com>
2023-11-05 11:22:35 +0900

    Revert ":bug: [FIX] 레시피 리스트 화면 복귀 시 좋아요 및 스크랩 미반영 버그 해결"

--- app/src/main/java/com/zipdabang/zipdabang_android/core/navigation/RecipeNavGraph.kt
+++ app/src/main/java/com/zipdabang/zipdabang_android/core/navigation/RecipeNavGraph.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
@@ -17,7 +16,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import androidx.navigation.navigation
 import androidx.paging.compose.collectAsLazyPagingItems
-import com.zipdabang.zipdabang_android.common.rememberLifecycleEvent
 import com.zipdabang.zipdabang_android.core.data_store.proto.CurrentPlatform
 import com.zipdabang.zipdabang_android.module.item.recipe.common.RecipeSort
 import com.zipdabang.zipdabang_android.module.item.recipe.common.RecipeSubtitleState
@@ -115,8 +113,6 @@ fun NavGraphBuilder.RecipeNavGraph(
             val viewModel = hiltViewModel<RecipeListViewModel>()
             val scope = rememberCoroutineScope()
 
-            val lifecycleEvent = rememberLifecycleEvent()
-
             val currentPlatform = viewModel.currentPlatform.value
             val total = viewModel.total.value.toString()
             val sortBy = viewModel.sortBy.collectAsState().value
@@ -126,17 +122,13 @@ fun NavGraphBuilder.RecipeNavGraph(
                 ownerType =  backStackEntry.arguments?.getString("ownerType")
             )
 
-            LaunchedEffect(key1 = lifecycleEvent) {
+            LaunchedEffect(key1 = true) {
                 viewModel.setSortBy("latest")
 
-                if (lifecycleEvent == Lifecycle.Event.ON_RESUME) {
-                    if (categoryState.categoryId == -1 && categoryState.ownerType != null) {
-                        viewModel.getOwnerItemCount(categoryState.ownerType)
-                        viewModel.refreshOwnerItems()
-                    } else {
-                        viewModel.getCategoryItemCount(categoryState.categoryId!!)
-                        viewModel.refreshCategoryItems()
-                    }
+                if (categoryState.categoryId == -1 && categoryState.ownerType != null) {
+                    viewModel.getOwnerItemCount(categoryState.ownerType)
+                } else {
+                    viewModel.getCategoryItemCount(categoryState.categoryId!!)
                 }
             }
 
